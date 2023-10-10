c66aee09b851962b8ee00bf1053be5d76f2bb746
codespace-college <145272503+codespace-college@users.noreply.github.com>
2023-10-10 10:33:45 +0000

    Main

--- age_calculator.java
+++ age_calculator.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import java.time.LocalDate;
 import java.time.Period;
 
-public class Main {
+public class age_calculator {
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
